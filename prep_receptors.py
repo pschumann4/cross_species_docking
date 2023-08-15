70338639aa96b7bef5c8e65929c4a59b9dd1bb20
Schumann <Schumann.Peter@epa.gov>
2023-08-15 16:36:40 -0500

    preps pdbqt and flex and rigid files in one loop

--- prep_receptors.py
+++ prep_receptors.py
@@ -1,8 +1,8 @@
 """
- This function loops the prepare_flexreceptor.py script over a specified 
- directory of PDB files to create the .pdbqt files for the rigid 
- and flexible residues.                                  
- requires: prepare_flexreceptor.py script, flex_residues.txt file, 
+ This function loops the prep_receptor4.py and prepare_flexreceptor.py 
+ scripts over a specified directory of PDB files to create the .pdbqt files 
+ for the rigid and flexible residues (if flexible docking is desired).                                  
+ requires: prep_receptor4.py, prepare_flexreceptor.py, flex_residues.txt, 
  and a directory of PDB files   
  returns: .pdbqt files for the rigid and flexible residues for each PDB 
  file in the directory     
@@ -88,18 +88,18 @@ def prep_receptors():
         pdbqt_files = os.listdir()
         pdbqt_files = [f for f in pdbqt_files if f.endswith(".pdbqt")]
 
-        if not os.path.exists("prepare_flexreceptors.py"):
+        if not os.path.exists("prepare_flexreceptor.py"):
             flex_script = input(
-                "Enter the path to the prepare_flexreceptors.py script: "
+                "Enter the path to the prepare_flexreceptor.py script: "
             )
             flex_script = flex_script.replace('"', "")
             while not os.path.exists(flex_script):
                 flex_script = input(
                     "That path does not appear to exist.\nPlease enter the path to "
-                    "the prepare_flexreceptors.py script: "
+                    "the prepare_flexreceptor.py script: "
                 )
         else:
-            flex_script = "prepare_flexreceptors.py"
+            flex_script = "prepare_flexreceptor.py"
 
         if not flex_script.startswith('"') and not flex_script.endswith('"'):
             flex_script = '"' + flex_script + '"'
@@ -119,25 +119,31 @@ def prep_receptors():
                     residues = residues[1:-1]
         else:
             residues = "flex_residues.txt"
+        if not flex_script.startswith('"') and not flex_script.endswith('"'):
+            flex_script = '"' + flex_script + '"'
         # Read the flex_residues.txt file
         with open(residues, "r") as f:
             res_list = f.readlines()
             # Remove header and footer lines
             res_list = res_list[2:-2]
-        # Initialize a list to track failed preps
+
+       # Initialize a list to track failed preps
         failed = []
         # Initialize list to track PDBQT files that have no flexible residues
         no_flex = []
         # Create a folder for the failed preps
         if not os.path.exists("failed_preps"):
             os.mkdir("failed_preps")
-        # Loop the prepare_flexreceptor.py script over the PDBQT files
+
+        # Prepare the flexible residues for each PDBQT file
         for pdbqt in pdbqt_files:
-            # Get the name of the PDBQT file
+            # temp_pdbqt is a list of the lines in the PDBQT file that are
+            # within +/- 5 residues of the flexible residues
+            temp_pdbqt = []
+            # res_temp is a list of the res range to subset the PDBQT file
+            res_temp = []
             pdbqt_name = pdbqt.split(".pdbqt")[0]
             print("\nPreparing " + pdbqt_name + "...")
-            # Match the PDBQT file name to the PDBQT ID in the flex_residues.txt
-            # file and get the residues to set as flexible
             for line in res_list:
                 if line.split(":")[0] == pdbqt_name:
                     res = line.split("[")[1].split("]")[0]
@@ -146,28 +152,80 @@ def prep_receptors():
                         print("No flexible residues for " + pdbqt_name)
                         no_flex.append(pdbqt_name)
                         continue
-                    # Replace the commas with underscores
+                    # Subset the PDBQT to include +/- 5 residues based on the
+                    # first and last residue in the flexible residues list
+                    res_id = res.split(",")
+                    res_id = [i[3:] for i in res_id] # Remove the res name
+                    res_id = sorted(res_id, key=lambda x: int(x))
+                    res_id.insert(0, str(int(res_id[0]) - 5))
+                    res_id.append(str(int(res_id[-1]) + 5))
+                    for i in range(int(res_id[0]), int(res_id[-1]) + 1):
+                        res_temp.append(str(i))
+                    with open(pdbqt, "r") as f:
+                        pdbqt_lines = f.readlines()
+                        for line in pdbqt_lines:
+                            if line[22:26].strip() in res_temp:
+                                temp_pdbqt.append(line)
+                    # The subsetted PDBQT file is written to a temp file so that it
+                    # can be used as the input for the prepare_flexreceptor.py script
+                    with open(pdbqt_name + "_temp.pdbqt", "w") as f:
+                        for line in temp_pdbqt:
+                            f.write(line)
                     res = res.replace(",", "_")
-                    # Create the command to run the prepare_flexreceptor.py script
-                    cmd = "python {} -r {}.pdbqt -s {} -g {}_rigid_residues.pdbqt -x {}_flex_residues.pdbqt -v".format(flex_script, pdbqt_name, res, pdbqt_name, pdbqt_name)
+                    cmd = "python {} -r {}_temp.pdbqt -s {} -g {}_rigid_residues.pdbqt -x {}_flex_residues.pdbqt -v".format(flex_script, pdbqt_name, res, pdbqt_name, pdbqt_name)
                     os.system(cmd)
-                    # If there are no flex_residues and rigid_residues .pdbqt files, add the PDBQT file name to the failed list
                     if (
                         not os.path.exists(pdbqt_name + "_flex_residues.pdbqt")
                         and not os.path.exists(pdbqt_name + "_rigid_residues.pdbqt")
                         and pdbqt_name not in no_flex
                     ):
                         failed.append(pdbqt_name)
-                        # Move the PDB file to the failed_preps folder
                         shutil.move(pdbqt, "failed_preps\\" + pdbqt)
+
+        # Remove all of the temp_pdbqt files
+        temp_files = os.listdir()
+        temp_files = [f for f in temp_files if f.endswith("_temp.pdbqt")]
+        for f in temp_files:
+            os.remove(f)
+        
+        # Create a new rigid receptor file by merging the original PDBQT file
+        # with the rigid residues file
+        pdbqt_files = os.listdir()
+        for pdbqt in pdbqt_files:
+            new_file = []
+            if pdbqt.endswith("modified.pdbqt"):
+                print("\nUpdating rigid receptor file for " + pdbqt.split("_", 1)[0] + "...")
+                with open(pdbqt, "r") as f:
+                    modified = f.readlines()
+                # Match the name of the PDBQT file to the name of the rigid residues file
+                species = pdbqt.split("_", 1)[0]
+                rigid_file = [f for f in pdbqt_files if f.startswith(species) and f.endswith("rigid_residues.pdbqt")]
+                for file in rigid_file:
+                    with open(file, "r") as f:
+                        rigid_pqbqt = f.readlines()
+                        rigid_residues = [i[22:26].strip() for i in rigid_pqbqt if i.startswith("ATOM")]
+                    # Remove the lines of the residues of the modified PDBQT file that are in the rigid residues file
+                    modified = [i for i in modified if i[22:26].strip() not in rigid_residues]
+                    # Add all the remaining lines from the modified PDBQT file and the rigid residues file to a new list
+                    for line in modified:
+                        new_file.append(line)
+                    with open(rigid_file[0], "r") as f:
+                        rigid = f.readlines()
+                        for line in rigid:
+                            new_file.append(line)
+                    # Overwrite the original rigid residues file with the new file
+                    with open(rigid_file[0], "w") as f:
+                        for line in new_file:
+                            f.write(line)
+
         # Print the list of failed preps
         if len(failed) > 0:
-            print("The following PDBQT files failed to prep:")
+            print("\nThe following PDBQT files failed to prep:")
             for fail in failed:
                 print(fail)
-            print("Consider a manual prep using the AutoDockTools GUI.")
+            print("\nConsider a manual prep using the AutoDockTools GUI.")
         else:
-            print("All PDBQT files were successfully prepped.")
+            print("\nAll PDBQT files were successfully prepped.")
     else:
         pass
 
